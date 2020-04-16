2800eb5a6bea909c758e9eb1b93899dec2f7c8be
Elad <eladnah@gmail.com>
2020-04-16 07:35:24 +0300

    main: load model checkpoint first to update model hps, set seed, create optimizer, load checkpoint state into optimizer

--- ddfa.py
+++ ddfa.py
@@ -1,10 +1,19 @@
+import numpy as np
 import torch
+import torch.nn as nn
+from torch.optim.optimizer import Optimizer
 from torch.utils.data.dataloader import DataLoader
 
-from execution_parameters import ModelExecutionParams
-from code_tasks.code_tasks import load_task
-from dataset_properties import DataFold
-from nn_utils import fit
+from ddfa.execution_parameters import ModelExecutionParams
+from ddfa.ddfa_model_hyper_parameters import DDFAModelTrainingHyperParams
+from ddfa.code_tasks.code_task_base import CodeTaskBase
+from ddfa.dataset_properties import DataFold
+from ddfa.nn_utils import fit
+
+
+def create_optimizer(model: nn.Module, train_hps: DDFAModelTrainingHyperParams) -> Optimizer:
+    # TODO: fully implement (choose optimizer and lr)!
+    return torch.optim.adam.Adam(model.parameters())
 
 
 def main():
@@ -12,13 +21,36 @@ def main():
         load_from_args=True, load_from_yaml=True, verify_confclass=True)
     device = torch.device("cuda" if exec_params.use_gpu_if_available and torch.cuda.is_available() else "cpu")
 
-    task = load_task(exec_params.experiment_setting.task)
+    loaded_checkpoint = None
+    if exec_params.should_load_model:
+        loaded_checkpoint = torch.load(exec_params.model_load_path)
+        # TODO: Modify `exec_params.experiment_setting` according to `loaded_checkpoint['experiment_setting']`.
+        #       Verify overridden arguments and raise ArgumentException if needed.
+
+    if exec_params.seed is not None:
+        np.random.seed(exec_params.seed)
+        torch.manual_seed(exec_params.seed)
+
+    task = CodeTaskBase.load_task(exec_params.experiment_setting.task)
+
+    if exec_params.perform_preprocessing:
+        task.preprocess(
+            model_hps=exec_params.experiment_setting.model_hyper_params,
+            pp_data_path=exec_params.pp_data_dir_path,
+            raw_train_data_path=exec_params.raw_train_data_path,
+            raw_eval_data_path=exec_params.raw_eval_data_path,
+            raw_test_data_path=exec_params.raw_test_data_path)
+
     model = task.build_model(exec_params.experiment_setting.model_hyper_params)
 
-    if exec_params.should_load_model:
-        raise NotImplementedError()  # TODO: implement!
+    if loaded_checkpoint:
+        model.load_state_dict(loaded_checkpoint['model_state_dict'])
 
     if exec_params.perform_training:
+        optimizer = create_optimizer(model, exec_params.experiment_setting.train_hyper_params)
+        if loaded_checkpoint:
+            optimizer.load_state_dict(loaded_checkpoint['optimizer_state_dict'])
+
         train_dataset = task.create_dataset(
             model_hps=exec_params.experiment_setting.model_hyper_params,
             dataset_props=exec_params.experiment_setting.dataset,
@@ -32,17 +64,20 @@ def main():
                 model_hps=exec_params.experiment_setting.model_hyper_params,
                 dataset_props=exec_params.experiment_setting.dataset,
                 datafold=DataFold.Validation,
-                dataset_path=exec_params.train_data_path)
+                dataset_path=exec_params.eval_data_path)
             eval_loader = DataLoader(
                 eval_dataset, batch_size=exec_params.experiment_setting.train_hyper_params.batch_size * 2)
+
+        criterion = nn.NLLLoss()  # TODO: fully implement loss function selection!
+
         fit(
             nr_epochs=exec_params.experiment_setting.train_hyper_params.nr_epochs,
             model=model,
             device=device,
             train_loader=train_loader,
             valid_loader=eval_loader,
-            optimizer=None,  # TODO: implement!
-            criterion=None  # TODO: implement!
+            optimizer=optimizer,
+            criterion=criterion
         )
 
     if exec_params.perform_evaluation:  # TODO: consider adding `and not exec_params.perform_training`
