ecdab4e6d7c25d54bd537a4bce098d41c5e56c2a
Elad <eladnah@gmail.com>
2020-04-16 16:16:09 +0300

    identifier encoder impl using transformer encoder layers

--- ddfa/code_nn_modules/identifier_encoder.py
+++ ddfa/code_nn_modules/identifier_encoder.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from typing import Any
+from torch.nn.modules.transformer import TransformerEncoderLayer, TransformerEncoder, LayerNorm
 
 from ddfa.code_nn_modules.vocabulary import Vocabulary
 
@@ -9,6 +9,10 @@ class IdentifierEncoder(nn.Module):
     def __init__(self, sub_identifiers_vocab: Vocabulary):
         super(IdentifierEncoder, self).__init__()
         self.sub_identifiers_vocab = sub_identifiers_vocab
+        transformer_encoder_layer = TransformerEncoderLayer(d_model=256, nhead=1, dim_feedforward=1028)
+        encoder_norm = LayerNorm(256)
+        self.transformer_encoder = TransformerEncoder(
+            encoder_layer=transformer_encoder_layer, num_layers=3, norm=encoder_norm)
 
-    def forward(self, *x: Any):
-        raise NotImplementedError()  # TODO: implement
+    def forward(self, sub_identifiers: torch.Tensor):
+        return self.transformer_encoder(sub_identifiers).sum(dim=0)
