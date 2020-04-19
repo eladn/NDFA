c1c86933238c641fa94f8a7725850a0fec10d864
Elad <eladnah@gmail.com>
2020-04-19 14:07:09 +0300

    cfg node encoder: fix batch handling (flatten & reshape)

--- ddfa/code_nn_modules/cfg_node_encoder.py
+++ ddfa/code_nn_modules/cfg_node_encoder.py
@@ -10,13 +10,18 @@ class CFGNodeEncoder(nn.Module):
         super(CFGNodeEncoder, self).__init__()
         self.expression_encoder = expression_encoder
         self.pdg_node_control_kinds_vocab_size = len(pdg_node_control_kinds_vocab)
+        self.pdg_node_control_kinds_embedding_dim = pdg_node_control_kinds_embedding_dim
         self.pdg_node_control_kinds_embeddings = nn.Embedding(
             num_embeddings=self.pdg_node_control_kinds_vocab_size,
-            embedding_dim=pdg_node_control_kinds_embedding_dim)
+            embedding_dim=self.pdg_node_control_kinds_embedding_dim)
 
     def forward(self, encoded_identifiers: torch.Tensor, cfg_nodes_expressions: torch.Tensor,
                 cfg_nodes_control_kind: torch.Tensor):
         encoded_expressions = self.expression_encoder(
             expressions=cfg_nodes_expressions, encoded_identifiers=encoded_identifiers)
-        embedded_cfg_nodes_control_kind = self.pdg_node_control_kinds_embeddings(cfg_nodes_control_kind)
-        return torch.cat([encoded_expressions, embedded_cfg_nodes_control_kind], dim=-1)
+        assert len(cfg_nodes_control_kind.size()) == 2  # (batch_size, nr_cfg_nodes)
+        embedded_cfg_nodes_control_kind = self.pdg_node_control_kinds_embeddings(cfg_nodes_control_kind.flatten())\
+            .view(cfg_nodes_control_kind.size() + (-1,))  # (batch_size, nr_cfg_nodes, control_kind_embedding)
+        assert len(encoded_expressions.size()) == len(embedded_cfg_nodes_control_kind.size()) == 3
+        assert encoded_expressions[:-1] == embedded_cfg_nodes_control_kind[:-1]
+        return torch.cat([encoded_expressions, embedded_cfg_nodes_control_kind], dim=-1)  # (batch_size, nr_cfg_nodes, expr_embed_dim + control_kind_embedding)
