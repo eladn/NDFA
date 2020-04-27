62db8457d5e8bb5767ebda86f626031966c75d7c
Elad <eladnah@gmail.com>
2020-04-27 22:47:30 +0300

    dataset props: fix enum bug (should not use commas between values); make datafold field optional

--- ddfa/dataset_properties.py
+++ ddfa/dataset_properties.py
@@ -1,17 +1,19 @@
 from confclass import confclass, confparam
 from enum import Enum
+from typing import Optional
 
 
 class DataFold(Enum):
-    Train = 'Train',
-    Validation = 'Validation',
+    Train = 'Train'
+    Validation = 'Validation'
     Test = 'Test'
 
 
 @confclass
 class DatasetProperties:
     name: str
-    datafold: str = confparam(
+    datafold: Optional[str] = confparam(
+        default=None,
         choices=('train', 'eval', 'test'))
 
     # TODO: add filters properties ...
