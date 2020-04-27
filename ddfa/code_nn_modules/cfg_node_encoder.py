829ea9d1ca51c1bbad6507cda7a5573520b8dc92
Elad <eladnah@gmail.com>
2020-04-27 22:41:18 +0300

    cfg node embeddings: fix minor assert bug

--- ddfa/code_nn_modules/cfg_node_encoder.py
+++ ddfa/code_nn_modules/cfg_node_encoder.py
@@ -24,5 +24,5 @@ class CFGNodeEncoder(nn.Module):
         embedded_cfg_nodes_control_kind = self.pdg_node_control_kinds_embeddings(cfg_nodes_control_kind.flatten())\
             .view(cfg_nodes_control_kind.size() + (-1,))  # (batch_size, nr_cfg_nodes, control_kind_embedding)
         assert len(encoded_expressions.size()) == len(embedded_cfg_nodes_control_kind.size()) == 3
-        assert encoded_expressions[:-1] == embedded_cfg_nodes_control_kind[:-1]
+        assert encoded_expressions.size()[:-1] == embedded_cfg_nodes_control_kind.size()[:-1]
         return torch.cat([encoded_expressions, embedded_cfg_nodes_control_kind], dim=-1)  # (batch_size, nr_cfg_nodes, expr_embed_dim + control_kind_embedding)
