13281df7b1a1f1a5f5ab6b06844deb5ac6676298
Elad Nachmias <eladnah@gmail.com>
2020-05-16 21:28:45 +0300

    fix notify train callback bugs (now works); fix bug at symbols set evaluation metric

--- ddfa/nn_utils/notify_train_callback.py
+++ ddfa/nn_utils/notify_train_callback.py
@@ -10,7 +10,7 @@ __all__ = ['NotifyCallback']
 
 
 class NotifyCallback(TrainCallback):
-    def __init__(self, notify_every_mins: Optional[int] = 5):
+    def __init__(self, notify_every_mins: Optional[int] = 10):
         self.notify_every_mins = notify_every_mins
 
         self.low_verbosity_notify = Notify()
@@ -30,9 +30,12 @@ class NotifyCallback(TrainCallback):
         if self.high_verbosity_notify is None:
             return
         if self.high_verbosity_last_msg_time is None or \
-                self.high_verbosity_last_msg_time + self.notify_every_mins >= time.time():
-            self.low_verbosity_notify.send(
-                f'Ep {epoch_nr} step {step_nr}/{nr_steps}: ')
+                self.high_verbosity_last_msg_time + self.notify_every_mins * 60 <= time.time():
+            self.high_verbosity_notify.send(
+                f'Ep {epoch_nr} step {step_nr}/{nr_steps}: '
+                f'loss (epoch avg): {epoch_avg_loss:.4f} -- '
+                f'loss (win avg): {epoch_moving_win_loss.get_window_avg():.4f} -- '
+                f'loss (win stbl avg): {epoch_moving_win_loss.get_window_avg_wo_outliers():.4f}')
             self.high_verbosity_last_msg_time = time.time()
 
     def epoch_end_after_evaluation(self, epoch_nr: int, epoch_avg_loss: float, epoch_moving_win_loss: WindowAverage,
