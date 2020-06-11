0e974eb9649db398c16360a4c7f8e1debf1013cc
Elad <eladnah@gmail.com>
2020-06-11 17:54:27 +0300

    notify train callback: start impl background worker; for now - only wrap send() with try.

--- ddfa/nn_utils/notify_train_callback.py
+++ ddfa/nn_utils/notify_train_callback.py
@@ -1,12 +1,57 @@
 import time
 from notify_run import Notify
 from typing import Dict, Optional
+import multiprocessing as mp
 
 from ddfa.nn_utils.train_callback import TrainCallback
 from ddfa.nn_utils.window_average import WindowAverage
 
 
 __all__ = ['NotifyCallback']
+
+
+class CloseNotifyProcessAction:
+    pass
+
+
+def _notify_process_worker(msgs_queue: mp.Queue, notify_worker_status_queue: mp.Queue):
+    notify = Notify()
+    notify.register()
+    msgs_queue.put(notify.endpoint, block=True)
+    while True:
+        msg_to_send = msgs_queue.get(block=True)
+        # maybe clear queue and get only last msg? (with nowait method within a loop)
+        if isinstance(msg_to_send, CloseNotifyProcessAction):
+            break
+        assert isinstance(msg_to_send, str)
+        try:
+            notify.send(msg_to_send)
+        except Exception as e:
+            print(f'Could not send notify message')
+
+
+class NotifyChannel:
+    def __init__(self):
+        # self.pipe_end, worker_pipe_end = mp.Pipe()
+        self.msgs_queue = mp.Queue()
+        self.notify_worker_status_queue = mp.Queue()
+        self.notify_process = mp.Process(
+            target=_notify_process_worker, args=(self.msgs_queue, self.notify_worker_status_queue))
+        self.notify_endpoint = None
+
+    def start(self):
+        self.notify_endpoint = self.msgs_queue.get(block=True)
+        self.notify_process.start()
+
+    def send(self, msg):
+        self.msgs_queue.put_nowait(msg)
+
+    def close(self):
+        self.msgs_queue.put(CloseNotifyProcessAction, block=True)
+        self.notify_process.join()
+        self.notify_process.close()
+        self.msgs_queue.close()
+        self.notify_worker_status_queue.close()
 
 
 class NotifyCallback(TrainCallback):
@@ -31,11 +76,14 @@ class NotifyCallback(TrainCallback):
             return
         if self.high_verbosity_last_msg_time is None or \
                 self.high_verbosity_last_msg_time + self.notify_every_mins * 60 <= time.time():
-            self.high_verbosity_notify.send(
-                f'Ep {epoch_nr} step {step_nr}/{nr_steps}: '
-                f'loss (epoch avg): {epoch_avg_loss:.4f} -- '
-                f'loss (win avg): {epoch_moving_win_loss.get_window_avg():.4f} -- '
-                f'loss (win stbl avg): {epoch_moving_win_loss.get_window_avg_wo_outliers():.4f}')
+            msg = f'Ep {epoch_nr} step {step_nr}/{nr_steps}: '\
+                  f'loss (epoch avg): {epoch_avg_loss:.4f} -- '\
+                  f'loss (win avg): {epoch_moving_win_loss.get_window_avg():.4f} -- '\
+                  f'loss (win stbl avg): {epoch_moving_win_loss.get_window_avg_wo_outliers():.4f}'
+            try:
+                self.high_verbosity_notify.send(msg)
+            except:
+                pass
             self.high_verbosity_last_msg_time = time.time()
 
     def epoch_end_after_evaluation(self, epoch_nr: int, epoch_avg_loss: float, epoch_moving_win_loss: WindowAverage,
@@ -44,8 +92,14 @@ class NotifyCallback(TrainCallback):
               f'\n\t validation loss: {validation_loss:.4f}' \
               f'\n\t validation metrics: {validation_metrics_results}'
         if self.high_verbosity_notify:
-            self.high_verbosity_notify.send(msg)
-        self.low_verbosity_notify.send(msg)
+            try:
+                self.high_verbosity_notify.send(msg)
+            except:
+                pass
+        try:
+            self.low_verbosity_notify.send(msg)
+        except:
+            pass
 
     def step_end_after_evaluation(self, epoch_nr: int, step_nr: int, nr_steps: int, batch_loss: float,
                                   batch_nr_examples: int, epoch_avg_loss: float, epoch_moving_win_loss: WindowAverage,
@@ -54,5 +108,11 @@ class NotifyCallback(TrainCallback):
               f'\n\t validation loss: {validation_loss:.4f}' \
               f'\n\t validation metrics: {validation_metrics_results}'
         if self.high_verbosity_notify:
-            self.high_verbosity_notify.send(msg)
-        self.low_verbosity_notify.send(msg)
+            try:
+                self.high_verbosity_notify.send(msg)
+            except:
+                pass
+        try:
+            self.low_verbosity_notify.send(msg)
+        except:
+            pass
