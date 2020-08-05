03b2060956acf6fda4747529f4ecc6d3b77842e3
Elad <eladnah@gmail.com>
2020-08-05 18:12:20 +0300

    unflatten_batch: fix new tensors device

--- ddfa/nn_utils/unflatten_batch.py
+++ ddfa/nn_utils/unflatten_batch.py
@@ -8,8 +8,7 @@ def unflatten_batch(flattened_data: torch.Tensor, examples_indices: torch.LongTe
         -> Tuple[torch.Tensor, torch.LongTensor, torch.BoolTensor]:
     assert len(flattened_data.size()) == 1
     assert len(examples_indices.size()) == 1
-    # TODO: set device like input for new created tensors!
-    batch_range = torch.arange(1, examples_indices.size()[0] + 1)
+    batch_range = torch.arange(1, examples_indices.size()[0] + 1, device=flattened_data.device)
     indices_of_last_item_per_example = batch_range[
         examples_indices != torch.cat((examples_indices[1:], examples_indices[-1].unsqueeze(-1) + 1))]
     nr_items_per_example = indices_of_last_item_per_example - torch.cat((torch.tensor([0]), indices_of_last_item_per_example[:-1]))
@@ -20,5 +19,7 @@ def unflatten_batch(flattened_data: torch.Tensor, examples_indices: torch.LongTe
     # pad_packed_sequence(packed_seq, batch_first=True)
     unflattened_data = pad_sequence(list_of_tensor_seq_per_example, batch_first=True)
     max_nr_items_per_example = unflattened_data.size()[1]
-    mask = nr_items_per_example.unsqueeze(-1).expand(batch_size, max_nr_items_per_example) >= torch.arange(1, max_nr_items_per_example + 1).unsqueeze(0).expand(batch_size, max_nr_items_per_example)
+    mask = nr_items_per_example.unsqueeze(-1).expand(batch_size, max_nr_items_per_example) >= \
+           torch.arange(1, max_nr_items_per_example + 1, device=flattened_data.device).unsqueeze(0)\
+               .expand(batch_size, max_nr_items_per_example)
     return pad_sequence(list_of_tensor_seq_per_example, batch_first=True), nr_items_per_example, mask
