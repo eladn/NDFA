91c951e959bb140fdb71c83c6ef700146d553f0d
Elad <eladnah@gmail.com>
2020-08-06 06:31:26 +0300

    unflatten_batch: impl with scatter and unflattened indices calculation

--- ddfa/nn_utils/unflatten_batch.py
+++ ddfa/nn_utils/unflatten_batch.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence
-from typing import Tuple
+from typing import Tuple, Optional
 
 
 def unflatten_batch(flattened_data: torch.Tensor, examples_indices: torch.LongTensor) \
@@ -23,3 +23,36 @@ def unflatten_batch(flattened_data: torch.Tensor, examples_indices: torch.LongTe
            torch.arange(1, max_nr_items_per_example + 1, device=flattened_data.device).unsqueeze(0)\
                .expand(batch_size, max_nr_items_per_example)
     return pad_sequence(list_of_tensor_seq_per_example, batch_first=True), nr_items_per_example, mask
+
+
+def unflatten_batch2(
+        flattened_data: torch.Tensor, examples_indices: torch.LongTensor,
+        batch_size: Optional[int] = None, batch_range: Optional[torch.Tensor] = None,
+        max_nr_items_per_example: Optional[int] = None) \
+        -> Tuple[torch.Tensor, torch.LongTensor, torch.BoolTensor]:
+    assert len(flattened_data.size()) == 1
+    assert len(examples_indices.size()) == 1
+    if batch_size is None:
+        batch_size = examples_indices[-1].item() + 1
+    if batch_range is None:
+        batch_range = torch.arange(1, examples_indices.size()[0] + 1, device=flattened_data.device)
+    last_item_per_example_mask = \
+        examples_indices != torch.cat((examples_indices[1:], examples_indices[-1].unsqueeze(-1) + 1))
+    first_item_per_example_mask = \
+        examples_indices != torch.cat((torch.tensor([-1]), examples_indices[:-1]))
+    indices_of_last_item_per_example = batch_range[last_item_per_example_mask]  # alt: last_item_per_example_mask.nonzeros().unsqueeze(-1) + 1
+    nr_items_per_example = \
+        indices_of_last_item_per_example - \
+        torch.cat((torch.tensor([0]), indices_of_last_item_per_example[:-1]))
+    if max_nr_items_per_example is None:
+        max_nr_items_per_example = torch.max(nr_items_per_example, dim=0).values.item()
+    nr_items_per_last_example_in_first_item_of_each_example = torch.zeros_like(examples_indices).masked_scatter_(
+        mask=first_item_per_example_mask,
+        source=torch.cat((torch.tensor([0], dtype=torch.long), nr_items_per_example[:-1]), dim=0))
+    range_per_example = batch_range - 1 + torch.cumsum(-nr_items_per_last_example_in_first_item_of_each_example, dim=0)
+    new_scatter_indices = examples_indices * max_nr_items_per_example + range_per_example
+    zeros = torch.zeros(
+        (batch_size * max_nr_items_per_example,), device=flattened_data.device, dtype=flattened_data.dtype)
+    return torch.scatter(
+        zeros, dim=0, index=new_scatter_indices, src=flattened_data)\
+        .view((batch_size, max_nr_items_per_example))
