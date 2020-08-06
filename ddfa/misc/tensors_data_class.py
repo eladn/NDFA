d6ecb513de47b69f4e136571fdc11f43091f138d
Elad <eladnah@gmail.com>
2020-08-06 10:26:51 +0300

    tensors_data_class: impl TensorWithCollateMask - allows tensor with various sizes across examples, and automatically creates mask when collating to batch

--- ddfa/misc/tensors_data_class.py
+++ ddfa/misc/tensors_data_class.py
@@ -1,9 +1,9 @@
 import torch
 import dataclasses
-from typing import List, Union, Iterable, Optional
+from typing import List, Union, Optional, Tuple, final
 
 
-__all__ = ['TensorsDataClass']
+__all__ = ['TensorsDataClass', 'TensorWithCollateMask']
 
 
 @dataclasses.dataclass
@@ -46,6 +46,33 @@ class TensorsDataClass:
         return new_obj
 
     @classmethod
+    def collate_field(
+            cls, field_name: str,
+            values_as_tuple: Tuple[Union[str, bool, dict, list, tuple, 'TensorsDataClass', torch.Tensor]]):
+        assert field_name not in {'_batch_size'}
+        assert all(type(value) == type(values_as_tuple[0]) for value in values_as_tuple)
+        if values_as_tuple[0] is None:
+            return None
+        if isinstance(values_as_tuple[0], TensorsDataClass):
+            assert hasattr(values_as_tuple[0].__class__, 'collate')
+            return values_as_tuple[0].__class__.collate(values_as_tuple)
+        if isinstance(values_as_tuple[0], dict):
+            all_keys = {key for dct in values_as_tuple for key in dct.keys()}
+            return {key: cls.collate_field(field_name, (dct[key] for dct in values_as_tuple if key in dct))
+                    for key in all_keys}
+        if isinstance(values_as_tuple[0], list) or isinstance(values_as_tuple[0], tuple):
+            collection_type = type(values_as_tuple[0])
+            assert all(len(lst) == len(values_as_tuple[0]) for lst in values_as_tuple)
+            return collection_type(cls.collate_field(field_name, vals) for vals in zip(*values_as_tuple))
+        if isinstance(values_as_tuple[0], torch.Tensor):
+            if any(tensor.size() != values_as_tuple[0].size() for tensor in values_as_tuple):
+                raise ValueError(
+                    f'Not all examples have the same tensor size for `{field_name}`. '
+                    f'sizes: {[tensor.size() for tensor in values_as_tuple]}')
+            return torch.cat(tuple(tensor.unsqueeze(0) for tensor in values_as_tuple), dim=0)
+        return values_as_tuple
+
+    @classmethod
     def collate(cls, inputs: List['TensorsDataClass']):
         assert all(isinstance(inp, cls) for inp in inputs)
         assert all(not inp.is_batched for inp in inputs)
@@ -64,32 +91,56 @@ class TensorsDataClass:
                     f'Not all examples have the same tensor size for `{field.name}`. '
                     f'sizes: {[getattr(inp, field.name).size() for inp in inputs]}')
 
-        def collate_field(field_name: str, values_iter: Iterable[Union[str, bool, dict, list, tuple, TensorsDataClass, torch.Tensor]]):
-            assert field_name not in {'_batch_size'}
-            values_as_tuple = tuple(values_iter)
-            assert all(type(value) == type(values_as_tuple[0]) for value in values_as_tuple)
-            if values_as_tuple[0] is None:
-                return None
-            if isinstance(values_as_tuple[0], TensorsDataClass):
-                assert hasattr(values_as_tuple[0].__class__, 'collate')
-                return values_as_tuple[0].__class__.collate(values_as_tuple)
-            if isinstance(values_as_tuple[0], dict):
-                all_keys = {key for dct in values_as_tuple for key in dct.keys()}
-                return {key: collate_field(field_name, (dct[key] for dct in values_as_tuple if key in dct))
-                        for key in all_keys}
-            if isinstance(values_as_tuple[0], list) or isinstance(values_as_tuple[0], tuple):
-                collection_type = type(values_as_tuple[0])
-                assert all(len(lst) == len(values_as_tuple[0]) for lst in values_as_tuple)
-                return collection_type(collate_field(field_name, vals) for vals in zip(*values_as_tuple))
-            if isinstance(values_as_tuple[0], torch.Tensor):
-                return torch.cat(tuple(tensor.unsqueeze(0) for tensor in values_as_tuple), dim=0)
-            return values_as_tuple
-
         assert all(field.init ^ (field.name in {'_batch_size'}) for field in dataclasses.fields(cls))
         batched_obj = cls(
-            **{field.name: collate_field(
-                field.name, (getattr(inp, field.name) for inp in inputs))
-                for field in dataclasses.fields(cls) if field.init and field.name not in {'_batch_size'}})
+            **{field.name: cls.collate_field(
+                field.name, tuple(getattr(inp, field.name) for inp in inputs))
+                for field in dataclasses.fields(cls)
+                if field.init and field.name not in {'_batch_size'}})
+        batched_obj._batch_size = len(inputs)
+        return batched_obj
+
+
+@final
+@dataclasses.dataclass
+class TensorWithCollateMask(TensorsDataClass):
+    tensor: torch.Tensor
+    collate_mask: torch.BoolTensor = dataclasses.field(default=None)
+
+    @classmethod
+    def collate(cls, inputs: List['TensorWithCollateMask']):
+        assert all(type(inp) == TensorWithCollateMask for inp in inputs)
+        assert all(isinstance(inp.tensor, torch.Tensor) for inp in inputs)
+        assert all(len(inp.tensor.size()) == len(inputs[0].tensor.size()) for inp in inputs)
+        assert all(inp.collate_mask is None for inp in inputs)
+
+        tensors = tuple(getattr(inp, 'tensor') for inp in inputs)
+        max_dims = tuple(max(tensor.size()[dim_idx] for tensor in tensors)
+                         for dim_idx in range(len(tensors[0].size())))
+        collate_size = (len(tensors),) + max_dims
+        collate_mask = torch.zeros(collate_size, dtype=torch.bool)
+        collated_tensor = torch.zeros(collate_size, dtype=tensors[0].dtype)
+        for idx, tensor in enumerate(tensors):
+            if len(tensor.size()) == 0:
+                collated_tensor[idx] = tensor
+                collate_mask[idx] = True
+            if len(tensor.size()) == 1:
+                collated_tensor[idx, :tensor.size()[0]] = tensor
+                collate_mask[idx, :tensor.size()[0]] = True
+            elif len(tensor.size()) == 2:
+                collated_tensor[idx, :tensor.size()[0], :tensor.size()[1]] = tensor
+                collate_mask[idx, :tensor.size()[0], :tensor.size()[1]] = True
+            elif len(tensor.size()) == 3:
+                collated_tensor[idx, :tensor.size()[0], :tensor.size()[1], :tensor.size()[2]] = tensor
+                collate_mask[idx, :tensor.size()[0], :tensor.size()[1], :tensor.size()[2]] = True
+            elif len(tensor.size()) == 4:
+                collated_tensor[idx, :tensor.size()[0], :tensor.size()[1], :tensor.size()[2], :tensor.size()[3]] = tensor
+                collate_mask[idx, :tensor.size()[0], :tensor.size()[1], :tensor.size()[2], :tensor.size()[3]] = True
+            else:
+                raise ValueError(f'Cannot collate tensor with >= 4 dims. '
+                                 f'Given input tensors with {len(tensor.size())} dims.')
+
+        batched_obj = TensorWithCollateMask(tensor=collated_tensor, collate_mask=collate_mask)
         batched_obj._batch_size = len(inputs)
         return batched_obj
 
