2226554b27cf7709926834aca5f9053b74a294ad
Elad <eladnah@gmail.com>
2020-09-10 10:18:55 +0300

    SeqContextAdder: add mask

--- ndfa/nn_utils/seq_context_adder.py
+++ ndfa/nn_utils/seq_context_adder.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import Optional
 
 
 __all__ = ['SeqContextAdder']
@@ -16,7 +17,8 @@ class SeqContextAdder(nn.Module):
         self.second_linear_layer = nn.Linear(in_features=self.main_dim, out_features=self.main_dim)
         self.dropout_layer = nn.Dropout(p=dropout_p)
 
-    def forward(self, sequence: torch.Tensor, context: torch.Tensor, batch_first: bool = True):
+    def forward(self, sequence: torch.Tensor, context: torch.Tensor,
+                sequence_mask: Optional[torch.BoolTensor] = None, batch_first: bool = True):
         assert batch_first
         assert sequence.ndim == 3
         assert sequence.size(2) == self.main_dim
@@ -26,4 +28,7 @@ class SeqContextAdder(nn.Module):
         ctx_expanded = context.unsqueeze(1).expand(batch_size, seq_len, self.ctx_dim)
         sequence_with_ctx = torch.cat([sequence, ctx_expanded], dim=-1)
         projected = self.dropout_layer(F.relu(self.first_projection_layer(sequence_with_ctx)))
-        return self.dropout_layer(F.relu(self.second_linear_layer(projected)))
+        final = self.dropout_layer(F.relu(self.second_linear_layer(projected)))
+        if sequence_mask is not None:
+            final = torch.zeros_like(final).masked_scatter(sequence_mask.unsqueeze(-1).expand(final.size()), final)
+        return final
