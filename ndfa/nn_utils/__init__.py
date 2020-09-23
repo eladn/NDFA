f229810a3e2836801e353e87686937c7e7c87e06
Elad <eladnah@gmail.com>
2020-09-23 23:19:13 +0300

    add tanh to possible activations

--- ndfa/nn_utils/__init__.py
+++ ndfa/nn_utils/__init__.py
@@ -1,9 +1,9 @@
-from . import misc
 from . import apply_batched_embeddings
 from . import attention
 from . import attn_rnn_decoder
 from . import attn_rnn_encoder
 from . import dbg_test_grads
+from . import misc
 from . import notify_train_callback
 from . import scattered_encodings
 from . import train_callback
@@ -12,12 +12,12 @@ from . import unflatten_batch
 from . import window_average
 
 __all__ = \
-    misc.__all__ + \
     apply_batched_embeddings.__all__ + \
     attention.__all__ + \
     attn_rnn_decoder.__all__ + \
     attn_rnn_encoder.__all__ + \
     dbg_test_grads.__all__ + \
+    misc.__all__ + \
     notify_train_callback.__all__ + \
     scattered_encodings.__all__ + \
     train_callback.__all__ + \
--- ndfa/nn_utils/misc.py
+++ ndfa/nn_utils/misc.py
@@ -1,12 +1,11 @@
 import torch
-import torch.nn.functional as F
 
 
 __all__ = ['get_activation']
 
 
 def get_activation(activation_str: str):
-    activations = {'relu': F.relu, 'prelu': F.prelu, 'sigmoid': torch.sigmoid}
+    activations = {'relu': torch.relu, 'prelu': torch.prelu, 'sigmoid': torch.sigmoid, 'tanh': torch.tanh}
     if activation_str not in activations:
         raise ValueError(
             f'`{activation_str}` is not a valid activation function (use one of these: {activations.keys()}).')
