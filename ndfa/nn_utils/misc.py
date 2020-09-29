ac69e3f8b6cbf234ac7827b5626219f1b76a84e2
Elad <eladnah@gmail.com>
2020-09-29 17:35:39 +0300

    activations: add `none` option for no activation later (identity)

--- ndfa/nn_utils/misc.py
+++ ndfa/nn_utils/misc.py
@@ -7,7 +7,9 @@ __all__ = ['get_activation_layer', 'get_activation_fn']
 
 
 def get_activation_layer(activation_str: str):
-    activation_layers = {'relu': nn.ReLU, 'prelu': nn.PReLU, 'leaky-relu': nn.LeakyReLU, 'sigmoid': nn.Sigmoid, 'tanh': nn.Tanh}
+    activation_layers = {
+        'relu': nn.ReLU, 'prelu': nn.PReLU, 'leaky-relu': nn.LeakyReLU,
+        'sigmoid': nn.Sigmoid, 'tanh': nn.Tanh, 'none': nn.Identity}
     if activation_str not in activation_layers:
         raise ValueError(
             f'`{activation_str}` is not a valid activation function (use one of these: {activation_layers.keys()}).')
@@ -15,7 +17,9 @@ def get_activation_layer(activation_str: str):
 
 
 def get_activation_fn(activation_str: str):
-    activation_fns = {'relu': torch.relu, 'prelu': torch.prelu, 'leaky-relu': F.leaky_relu, 'sigmoid': torch.sigmoid, 'tanh': torch.tanh}
+    activation_fns = {
+        'relu': torch.relu, 'prelu': torch.prelu, 'leaky-relu': F.leaky_relu,
+        'sigmoid': torch.sigmoid, 'tanh': torch.tanh, 'none': lambda x: x}
     if activation_str not in activation_fns:
         raise ValueError(
             f'`{activation_str}` is not a valid activation function (use one of these: {activation_fns.keys()}).')
