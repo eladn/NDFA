2fecf78a95114a36943ad2138fc3b085360e833e
Elad <eladnah@gmail.com>
2020-10-19 13:27:04 +0300

    scatter_attention: fix case where not all of the range [0, ..., attn_keys.size(0)] occur in `indices`; add TODO for additional verification

--- ndfa/nn_utils/scatter_attention.py
+++ ndfa/nn_utils/scatter_attention.py
@@ -31,18 +31,27 @@ class ScatterAttention(nn.Module):
             # means attn_keys_indices is [0, 1, 2, ..., max(indices)]
             # scores(i) = softmax(<scattered_values[j] * W_attn * attn_keys[i] | j's s.t. indices[j]=i>)
             # out[i] = scores(i) ELEM_MUL <scattered_values[j] | j's s.t. indices[j]=i>
+            nr_elements = attn_keys.size(0)
             scattered_attn_keys_projected = torch.gather(
                 attn_keys_projected, dim=0,
                 index=indices.unsqueeze(-1).expand(indices.size(0), attn_keys_projected.size(1)))
             assert scattered_attn_keys_projected.size() == scattered_values.size()
             scattered_probs = torch.sum(scattered_values * scattered_attn_keys_projected, dim=1)
             assert scattered_probs.ndim == 1 and scattered_probs.size() == (scattered_values.size(0),)
+            # TODO: Is it ok calling `scatter_softmax()` when not all of the range
+            #       [0, 1, 2, ..., attn_keys.size(0) - 1] occurs in `indices`?
+            #       What is entered to the missing-indices-entries in the output vector?
+            #       Are they zeroed (the good case) or just skipped (bad case because of
+            #       incorrect offsetting of the following entries)?
             scattered_scores = scatter_softmax(src=scattered_probs, index=indices, dim=-1)
             assert scattered_scores.ndim == 1 and scattered_scores.size() == (scattered_values.size(0),)
             scattered_values_weighed_by_scores = \
                 scattered_values * scattered_scores.unsqueeze(-1).expand(scattered_values.size())
             assert scattered_values_weighed_by_scores.size() == scattered_values.size()
-            attn_applied = scatter_sum(scattered_values_weighed_by_scores, index=indices, dim=0)
+            # Note: Passing `dim_size` to `scatter_sum()` is important for the case where
+            #       not all of the range [0, 1, 2, ..., attn_keys.size(0) - 1] occurs in `indices`.
+            attn_applied = scatter_sum(
+                scattered_values_weighed_by_scores, index=indices, dim=0, dim_size=nr_elements)
             return scattered_scores, attn_applied
         else:
             # scores(i) = softmax(<scattered_values[j] * W_attn * attn_keys[i] | j's s.t. indices[j]=attn_keys_indices[i]>)
