e9448374e1593cc814679b849ebf99e76c387fb6
Elad <eladnah@gmail.com>
2021-07-10 13:22:59 +0300

    symbol_occurrences_extractor_from_encoded_method: support all kinds of ast paths for set-of-paths ast encoding

--- ndfa/code_nn_modules/symbol_occurrences_extractor_from_encoded_method.py
+++ ndfa/code_nn_modules/symbol_occurrences_extractor_from_encoded_method.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-from typing import Tuple
 from warnings import warn
+from typing import Tuple, NamedTuple, Collection
 
 from ndfa.code_nn_modules.code_task_input import CodeExpressionTokensSequenceInputTensors, MethodASTInputTensors
 from ndfa.code_nn_modules.code_expression_encodings_tensors import CodeExpressionEncodingsTensors
@@ -10,6 +10,41 @@ from ndfa.nn_utils.functions.last_item_in_sequence import get_last_item_in_seque
 
 
 __all__ = ['SymbolOccurrencesExtractorFromEncodedMethod']
+
+
+class ScatteredOccurrences(NamedTuple):
+    encodings: torch.Tensor
+    indices: torch.LongTensor
+
+    def verify_matching_shapes(self):
+        assert self.encodings.shape[:-1] == self.indices.shape
+
+    @classmethod
+    def cat(cls, items: Collection['ScatteredOccurrences']) -> 'ScatteredOccurrences':
+        scattered_occ = cls(
+            encodings=torch.cat([scattered_occ.encodings for scattered_occ in items], dim=0),
+            indices=torch.cat([scattered_occ.indices for scattered_occ in items], dim=0))
+        scattered_occ.verify_matching_shapes()
+        return scattered_occ
+
+
+def get_symbols_occurrences_from_ast_nodes_occurrences(
+        method_ast_input: MethodASTInputTensors,
+        ast_nodes_occurrences: ScatteredOccurrences) \
+        -> ScatteredOccurrences:
+    ast_nodes_occurrences.verify_matching_shapes()
+    # TODO: delete cast to bool after we fix the pp input
+    ast_nodes_has_symbol_mask = \
+        method_ast_input.ast_nodes_has_symbol_mask.tensor.type(torch.bool)
+    symbol_leaves_mask = ast_nodes_has_symbol_mask[ast_nodes_occurrences.indices]
+    encodings_of_symbols_occurrences = ast_nodes_occurrences.encodings[symbol_leaves_mask]
+    ast_node_indices_of_symbols_occurrences = ast_nodes_occurrences.indices[symbol_leaves_mask]
+    symbols_indices_of_symbols_occurrences = \
+        method_ast_input.ast_nodes_symbol_idx.indices[ast_node_indices_of_symbols_occurrences]
+    symbols_occurrences = ScatteredOccurrences(
+        encodings=encodings_of_symbols_occurrences, indices=symbols_indices_of_symbols_occurrences)
+    symbols_occurrences.verify_matching_shapes()
+    return symbols_occurrences
 
 
 class SymbolOccurrencesExtractorFromEncodedMethod(nn.Module):
@@ -25,7 +60,7 @@ class SymbolOccurrencesExtractorFromEncodedMethod(nn.Module):
         if self.code_expression_encoder_params.encoder_type == 'ast':
             if self.code_expression_encoder_params.ast_encoder.encoder_type == 'set-of-paths':
                 assert code_expression_encodings.ast_paths_by_type is not None
-                leaves_encodings, leaves_indices = None, None
+                all_symbols_occurrences = []
                 for paths_type, ast_paths in code_expression_encodings.ast_paths_by_type.items():
                     if paths_type == 'leaf_to_leaf':
                         assert len(ast_paths.nodes_occurrences.shape) == 3
@@ -42,19 +77,38 @@ class SymbolOccurrencesExtractorFromEncodedMethod(nn.Module):
                         assert first_item_encoding.shape == last_item_encoding.shape
                         assert first_item_node_index.shape == last_item_node_index.shape
                         assert first_item_encoding.shape[:-1] == first_item_node_index.shape
-                        assert leaves_encodings is None and leaves_indices is None
-                        leaves_encodings = torch.cat([first_item_encoding, last_item_encoding], dim=0)
-                        leaves_indices = torch.cat([first_item_node_index, last_item_node_index], dim=0)
+                        ast_nodes_occurrences = ScatteredOccurrences(
+                            encodings=torch.cat([first_item_encoding, last_item_encoding], dim=0),
+                            indices=torch.cat([first_item_node_index, last_item_node_index], dim=0))
+                        ast_nodes_occurrences.verify_matching_shapes()
+                        all_symbols_occurrences.append(get_symbols_occurrences_from_ast_nodes_occurrences(
+                            method_ast_input=method_ast_input, ast_nodes_occurrences=ast_nodes_occurrences))
+                    elif paths_type == 'leaf_to_root':
+                        leaves_encodings = ast_paths.nodes_occurrences[:, 0, :]
+                        leaves_indices = method_ast_input.ast_leaf_to_root_paths_node_indices.sequences[:, 0]
+                        ast_nodes_occurrences = ScatteredOccurrences(
+                            encodings=leaves_encodings, indices=leaves_indices)
+                        ast_nodes_occurrences.verify_matching_shapes()
+                        all_symbols_occurrences.append(get_symbols_occurrences_from_ast_nodes_occurrences(
+                            method_ast_input=method_ast_input, ast_nodes_occurrences=ast_nodes_occurrences))
+                    elif paths_type in {'siblings_sequences', 'siblings_w_parent_sequences'}:
+                        node_indices = method_ast_input.siblings_sequences_node_indices \
+                            if paths_type == 'siblings_sequences' \
+                            else method_ast_input.siblings_w_parent_sequences_node_indices
+                        ast_nodes_occurrences = ScatteredOccurrences(
+                            encodings=ast_paths.nodes_occurrences[node_indices.sequences_mask],
+                            indices=node_indices.sequences[node_indices.sequences_mask])
+                        ast_nodes_occurrences.verify_matching_shapes()
+                        all_symbols_occurrences.append(get_symbols_occurrences_from_ast_nodes_occurrences(
+                            method_ast_input=method_ast_input, ast_nodes_occurrences=ast_nodes_occurrences))
                     else:
                         warn(f'Extracting symbol occurrences from ast paths of type '
                              f'`{paths_type}` is currently not supported.')
-                assert leaves_encodings is not None and leaves_indices is not None
-                ast_nodes_has_symbol_mask = method_ast_input.ast_nodes_has_symbol_mask.tensor.type(torch.bool)  # TODO: delete cast to bool after we fix the pp input
-                symbol_leaves_mask = ast_nodes_has_symbol_mask[leaves_indices]
-                encodings_of_symbols_occurrences = leaves_encodings[symbol_leaves_mask]
-                ast_node_indices_of_symbols_occurrences = leaves_indices[symbol_leaves_mask]
-                symbols_indices_of_symbols_occurrences = \
-                    method_ast_input.ast_nodes_symbol_idx.indices[ast_node_indices_of_symbols_occurrences]
+                assert len(all_symbols_occurrences) >= 1
+                all_symbols_occurrences = all_symbols_occurrences[0] if len(all_symbols_occurrences) == 1 else \
+                    ScatteredOccurrences.cat(all_symbols_occurrences)
+                all_symbols_occurrences.verify_matching_shapes()
+                encodings_of_symbols_occurrences, symbols_indices_of_symbols_occurrences = all_symbols_occurrences
             else:
                 assert self.code_expression_encoder_params.ast_encoder.encoder_type in {'tree', 'paths-folded'}
                 assert code_expression_encodings.ast_nodes is not None
