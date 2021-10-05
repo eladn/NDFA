1d49c7c09cb6ce537c807a4bea8d2bb28355a09e
Elad <eladnah@gmail.com>
2021-10-05 14:43:58 +0300

    ASTTreeLSTMEncoder: add norm

--- ndfa/code_nn_modules/ast_tree_lstm_encoder.py
+++ ndfa/code_nn_modules/ast_tree_lstm_encoder.py
@@ -6,6 +6,7 @@ from typing import Optional
 from ndfa.nn_utils.modules.dgl_tree_lstm import TreeLSTM
 from ndfa.code_nn_modules.code_expression_encodings_tensors import CodeExpressionEncodingsTensors
 from ndfa.nn_utils.modules.params.norm_wrapper_params import NormWrapperParams
+from ndfa.nn_utils.modules.norm_wrapper import NormWrapper
 
 
 __all__ = ['ASTTreeLSTMEncoder']
@@ -16,7 +17,7 @@ class ASTTreeLSTMEncoder(nn.Module):
             self,
             ast_node_embedding_dim: int,
             direction: str = 'root_to_leaves',
-            norm_params: Optional[NormWrapperParams] = None,  # TODO: use it!
+            norm_params: Optional[NormWrapperParams] = None,
             dropout_rate: float = 0.3):
         super(ASTTreeLSTMEncoder, self).__init__()
         self.ast_node_embedding_dim = ast_node_embedding_dim
@@ -26,6 +27,8 @@ class ASTTreeLSTMEncoder(nn.Module):
             node_embedding_size=ast_node_embedding_dim,
             cell_type='sum_children',
             dropout_rate=dropout_rate)
+        self.norm = None if norm_params is None else NormWrapper(
+            nr_features=ast_node_embedding_dim, params=norm_params)
 
     def forward(self, ast_nodes_embeddings: torch.Tensor, ast_batch: dgl.DGLGraph) -> CodeExpressionEncodingsTensors:
         assert ast_nodes_embeddings.ndim == 2
@@ -37,4 +40,6 @@ class ASTTreeLSTMEncoder(nn.Module):
             tree=ast_batch, h=h, c=c,
             direction=self.direction)
         assert new_node_encodings.shape == ast_nodes_embeddings.shape
+        if self.norm:
+            new_node_encodings = self.norm(new_node_encodings)
         return CodeExpressionEncodingsTensors(ast_nodes=new_node_encodings)
